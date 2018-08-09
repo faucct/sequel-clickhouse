ace9f1b13a417b3374fbbb985eb221cfaeba6376
Nikita Sokolov <faucct@gmail.com>
2018-08-09 16:27:01 +0300

    It should be possible to create tables

--- lib/sequel/clickhouse.rb
+++ lib/sequel/clickhouse.rb
@@ -8,5 +8,72 @@ module Sequel
   # @see https://github.com/jeremyevans/sequel
   # @see https://clickhouse.yandex
   module Clickhouse
+    class Database < Sequel::Database # rubocop:disable Style/Documentation
+      set_adapter_scheme :clickhouse
+
+      def execute(sql, **options)
+        synchronize(options[:server]) { |conn| conn.execute sql }
+      rescue ::Clickhouse::QueryError
+        raise DatabaseError
+      end
+
+      def connect(server)
+        ::Clickhouse.connect(server_opts(server))
+      end
+
+      def dataset_class_default
+        Dataset
+      end
+
+      def column_definition_primary_key_sql(*)
+      end
+
+      def column_definition_null_sql(*)
+      end
+
+      def create_table_sql(name, generator, options)
+        primary_key_columns =
+          generator.columns.select { |c| c[:primary_key] }.map { |column| quote_identifier(column[:name]) }
+        if primary_key_columns.empty?
+          super
+        else
+          super + " ENGINE = ReplacingMergeTree ORDER BY (#{primary_key_columns.join(',')})"
+        end
+      end
+
+      def type_literal_generic_string(column)
+        raise column.inspect if column.key?(:null)
+        if column[:text]
+          'String'
+        else
+          "FixedString(#{column[:size] || default_string_column_size})"
+        end
+      end
+
+      def identifier_input_method_default
+        nil
+      end
+
+      def schema_parse_table(table, dataset: nil, server: nil, **)
+        input_identifier_meth = input_identifier_meth(dataset)
+        result = synchronize(server) do |conn|
+          conn.query("DESCRIBE TABLE #{input_identifier_meth.call(table)}")
+        end
+        output_identifier_meth = output_identifier_meth(dataset)
+        result.map do |row|
+          attributes = result.names.zip(row).to_h
+          [output_identifier_meth.call(attributes['name']), type: attributes['type']]
+        end
+      end
+
+      def schema_column_type(type)
+        case type
+        when /^Nullable\((.+)\)$/ then
+          schema_column_type(Regexp.last_match(1))
+        else
+          super
+        end
+      end
+    end
   end
 end
--- spec/sequel/clickhouse_spec.rb
+++ spec/sequel/clickhouse_spec.rb
@@ -2,4 +2,20 @@ RSpec.describe Sequel::Clickhouse do
   it 'has a version number' do
     expect(Sequel::Clickhouse::VERSION).not_to be nil
   end
+
+  describe described_class::Database do
+    describe '#create_table' do
+      subject do
+        lambda do
+          connection.create_table :schema_migrations, engine: 'MergeTree(filename)' do
+            String :filename, primary_key: true
+          end
+          connection.schema(:schema_migrations)
+        end
+      end
+      let(:connection) { Sequel.connect(adapter: 'clickhouse', database: 'stat') }
+
+      it { is_expected.not_to raise_error }
+    end
+  end
 end
